d0b4157c4dd0f734572e08d5dce022657fe8790a
ysultanmuratov-cyber <ysultanmuratov@gmail.com>
2026-05-04 14:39:09 +0500

    Update 4dars.py

--- 4dars.py
+++ 4dars.py
@@ -218,7 +218,8 @@ if not st.session_state.logged_in:
     # 1. Foydalanuvchilar bazasi (Xohlagancha qo'shishingiz mumkin)
     users = {
         "Murat": "12062006",
-        "Nilufar": "Nilufar0455"
+        "Nilufar": "Nilufar0455",
+        "radjabboyeva_m": "12345678"
     }
     
     u_login = st.text_input("Foydalanuvchi nomi (Login):")
