86cfbf6696d918ef545990f595622cab900de9fd
ysultanmuratov-cyber <ysultanmuratov@gmail.com>
2026-05-13 11:17:21 +0500

    Update 4dars.py

--- 4dars.py
+++ 4dars.py
@@ -4840,7 +4840,8 @@ if not st.session_state.get('logged_in', False):
         "Samandarov": "Shoxrux06", "Xudayberganova": "Sevinch06", "Erkayev": "Akmal06", 
         "CharosD": "DCh07172005", "Rozimova": "Sevinch2", "MadrimovaG": "Gulshoda006", 
         "Rozmetov": "Bekchon05", "admin": "murat_admin", "Yuldashova":"Zuxra05", "otaboyeva": "sevinch01",
-        "iskandarova": "maftuna12", "qadamova": "aziza23", "urinbayev": "zafarbek77", "pulatova": "dilnoza34"
+        "iskandarova": "maftuna12", "qadamova": "aziza23", "urinbayev": "zafarbek77", "pulatova": "dilnoza34",
+        "rustambekov": "maqsadbek66"
     }
     
     u_login = st.text_input("Foydalanuvchi nomi (Login):")
